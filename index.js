febdaceb5a79b86147b3b0b07963cb0317bb1088
Wendi Fan <wendi.fan@softwire.com>
2019-06-26 00:17:05 +0100

    Calculator using Switch

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ console.log('Please enter an operator:');
 const op = readline.prompt();
 
 console.log('Please enter first number:');
-const n1 = readline.prompt();
+const n1 = parseFloat(readline.prompt());
 
 console.log('Please enter second number:');
-const n2 = readline.prompt();
+const n2 = parseFloat(readline.prompt());
 
-if (op === '+') {
-    answer = n1 + n2;
-} else if (op === '-') {
-    answer = n1 - n2;
-} else if (op === '*') {
-    answer = n1 * n2;
-} else if (op === '/') {
-    answer = n1/n2;
-} 
+switch (op) {
+    case '+':
+        answer = n1 + n2;
+        break;
+    case '-':
+        answer = n1 - n2;
+        break;
+    case '*':
+        answer = n1 * n2;
+        break;
+    case '/':
+        answer = n1 / n2;
+        break;
+    default:
+        console.log('You did not enter a valid operator.');
+}
+
 console.log(n1, op,  n2,  '=', answer);
 
