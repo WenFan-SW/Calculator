c91fd9fe1f333eade2c29806031a1898d1de55ab
Wendi Fan <wendi.fan@softwire.com>
2019-06-25 22:54:37 +0100

    Added basic multiplier

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Welcome to the calculator!');
+const readline = require('readline-sync');
+
+console.log('Please enter first number:');
+const n1 = readline.prompt();
+
+console.log('Please enter second number:');
+const n2 = readline.prompt();
+
+let product = n1*n2;
+
+console.log(n1, 'x',  n2,  '=', product);
