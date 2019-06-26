c4c9ea5dab640d36fc312d7e56e21f26c643cea8
Wendi Fan <wendi.fan@softwire.com>
2019-06-26 01:36:46 +0100

    Calculator with Loops

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ const readline = require('readline-sync');
 console.log('Please enter an operator:');
 const op = readline.prompt();
 
-console.log('Please enter first number:');
-const n1 = parseFloat(readline.prompt());
+console.log('How many numbers would you like to', op, '?');
+const noOfNos = readline.prompt();
 
-console.log('Please enter second number:');
-const n2 = parseFloat(readline.prompt());
+let numbers = [];
+for (let i = 0; i < noOfNos; i++) {
+    console.log('Please enter number', i+1, ':');
+    numbers.push(parseFloat(readline.prompt()));
+}
 
-if (op === '+') {
-    answer = n1 + n2;
-} else if (op === '-') {
-    answer = n1 - n2;
-} else if (op === '*') {
-    answer = n1 * n2;
-} else if (op === '/') {
-    answer = n1/n2;
-} 
-console.log(n1, op,  n2,  '=', answer);
+let answer = numbers[0]
+
+for (let i = 1; i < noOfNos; i++) {
+    if (op === '+') {
+        answer += numbers[i];
+    } else if (op === '-') {
+        answer -= numbers[i];
+    } else if (op === '*') {
+        answer *= numbers[i];
+    } else if (op === '/') {
+        answer /= numbers[i];
+    } 
+}
+
+
+console.log(answer);
